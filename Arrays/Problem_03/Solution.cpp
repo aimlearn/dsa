5e2cbe68f8e845cd0445fda26640f27c3ff2fb15
Arsh <arshdeepsp@gmail.com>
2022-02-25 02:01:08 -0800

    Solution 3 fixed

--- Arrays/Problem_03/Solution.cpp
+++ Arrays/Problem_03/Solution.cpp
@@ -7,7 +7,7 @@ vector<int> findSum(int nums[], int val, int length);
 
 int main() {
     int arr[] = {1,2,3,4,5,6};
-    vector<int> vec = findSum(arr, 11, 6);
+    vector<int> vec = findSum(arr, 6, 6);
     for (int i : vec) cout << i << "\t";
 }
 
@@ -15,12 +15,12 @@ vector<int> findSum(int nums[], int val, int length) {
     vector<int> vec;
     set<int> numset;
     for (int i = 0; i < length; i++) {
-        if (numset.find(nums[i]) == numset.end()) numset.insert(nums[i]);
         if (numset.find(val - nums[i]) != numset.end()) {
             vec.push_back(nums[i]);
             vec.push_back(val - nums[i]);
             break;
         }
+        if (numset.find(nums[i]) == numset.end()) numset.insert(nums[i]);
     }
     return vec;
 }
